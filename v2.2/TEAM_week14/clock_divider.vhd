36a917ba46bae6fbe2cc84d5a29d772b97b55800
Strdgenk <acaca1022@naver.com>
2026-05-11 22:37:17 +0900

    Create clock_divider.vhd

---
+++ v2.2/TEAM_week14/clock_divider.vhd
@@ -0,0 +1,28 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- 2학년 2반 2021041028 김호중, 2021041080 정승현
+-- 과제 1
+
+entity clock_divider is
+	 generic(count : integer range 0 to 50000000 := 4);
+    Port ( CLK_IN : in  STD_LOGIC;
+           CLK_OUT : out  STD_LOGIC);
+end clock_divider;
+
+architecture Behavioral of clock_divider is
+begin
+	process(CLK_IN)
+		variable tmp : integer range 0 to 50000000 := 0;
+	begin
+		if(CLK_IN'event and CLK_IN = '1') then
+			if(tmp = count-1) then
+				tmp := 0;
+				CLK_OUT <= '1';
+			else
+				tmp := tmp + 1;
+				CLK_OUT <= '0';
+			end if;
+		end if;
+	end process;
+end Behavioral;
